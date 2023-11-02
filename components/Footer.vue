c62732b79694f2312300e0ea7a1a324b0054808a
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2023-11-03 00:08:29 +0700

    add footer and notifications

---
+++ components/Footer.vue
@@ -0,0 +1,166 @@
+<script setup></script>
+
+<template>
+  <footer>
+    <div class="container-travelers px-5 md:px-5 lg:px-0">
+      <!-- FOOTER LIST -->
+      <div
+        class="flex items-start flex-wrap md:flex-wrap lg:flex-nowrap gap-y-8 gap-x-10 md:gap-x-20 lg:gap-x-14 xl:gap-x-20 pb-10 md:pb-[48px] lg:pb-[55px]"
+      >
+        <div>
+          <h2 class="font-medium text-lg md:text-xl lg:text-2xl text-[#1E1E1E]">
+            Help center
+          </h2>
+          <div class="mt-5">
+            <p class="mb-3.5 md:mb-5">
+              <a
+                href="/"
+                class="font-medium text-base md:text-lg text-[#8A8A8A] hover:text-[#1E1E1E]"
+                >FAQ</a
+              >
+            </p>
+            <p class="mb-3.5 md:mb-5">
+              <a
+                href="/"
+                class="font-medium text-base md:text-lg text-[#8A8A8A] hover:text-[#1E1E1E]"
+                >Privacy policy</a
+              >
+            </p>
+            <p class="mb-3.5 md:mb-5">
+              <a
+                href="/"
+                class="font-medium text-base md:text-lg text-[#8A8A8A] hover:text-[#1E1E1E]"
+                >Cookies policy</a
+              >
+            </p>
+            <p>
+              <a
+                href="/"
+                class="font-medium text-base md:text-lg text-[#8A8A8A] hover:text-[#1E1E1E]"
+                >Terms of use</a
+              >
+            </p>
+          </div>
+        </div>
+        <div>
+          <h2 class="font-medium text-lg md:text-xl lg:text-2xl text-[#1E1E1E]">
+            Company
+          </h2>
+          <div class="mt-5">
+            <p class="mb-3.5 md:mb-5">
+              <a
+                href="/"
+                class="font-medium text-base md:text-lg text-[#8A8A8A] hover:text-[#1E1E1E]"
+                >FAQ</a
+              >
+            </p>
+            <p class="mb-3.5 md:mb-5">
+              <a
+                href="/"
+                class="font-medium text-base md:text-lg text-[#8A8A8A] hover:text-[#1E1E1E]"
+                >About Us</a
+              >
+            </p>
+            <p class="mb-3.5 md:mb-5">
+              <a
+                href="/"
+                class="font-medium text-base md:text-lg text-[#8A8A8A] hover:text-[#1E1E1E]"
+                >Career</a
+              >
+            </p>
+            <p>
+              <a
+                href="/"
+                class="font-medium text-base md:text-lg text-[#8A8A8A] hover:text-[#1E1E1E]"
+                >Blog</a
+              >
+            </p>
+          </div>
+        </div>
+        <div>
+          <h2 class="font-medium text-lg md:text-xl lg:text-2xl text-[#1E1E1E]">
+            Destinations
+          </h2>
+          <div class="mt-5">
+            <p class="mb-3.5 md:mb-5">
+              <a
+                href="/"
+                class="font-medium text-base md:text-lg text-[#8A8A8A] hover:text-[#1E1E1E]"
+                >Country</a
+              >
+            </p>
+            <p class="mb-3.5 md:mb-5">
+              <a
+                href="/"
+                class="font-medium text-base md:text-lg text-[#8A8A8A] hover:text-[#1E1E1E]"
+                >City</a
+              >
+            </p>
+          </div>
+        </div>
+        <div>
+          <h2 class="font-medium text-lg md:text-xl lg:text-2xl text-[#1E1E1E]">
+            Become our partner
+          </h2>
+          <div class="mt-5">
+            <p class="mb-3.5 md:mb-5">
+              <a
+                href="/"
+                class="font-medium text-base md:text-lg text-[#8A8A8A] hover:text-[#1E1E1E]"
+                >Partner Hub</a
+              >
+            </p>
+            <p class="mb-3.5 md:mb-5">
+              <a
+                href="/"
+                class="font-medium text-base md:text-lg text-[#8A8A8A] hover:text-[#1E1E1E]"
+                >Advertise on Travelers</a
+              >
+            </p>
+            <p class="mb-3.5 md:mb-5">
+              <a
+                href="#"
+                class="font-medium text-[#8A8A8A] hover:text-[#1E1E1E]"
+                >Affiliate</a
+              >
+            </p>
+            <p class="mb-3.5 md:mb-5">
+              <a
+                href="/"
+                class="font-medium text-base md:text-lg text-[#8A8A8A] hover:text-[#1E1E1E]"
+                >Partner connectivity</a
+              >
+            </p>
+          </div>
+        </div>
+        <div>
+          <h2 class="font-medium text-lg md:text-xl lg:text-2xl text-[#1E1E1E]">
+            Download the app
+          </h2>
+          <div class="mt-5">
+            <p class="mb-3.5 md:mb-5">
+              <a
+                href="/"
+                class="font-medium text-base md:text-lg text-[#8A8A8A] hover:text-[#1E1E1E]"
+                >iOS app</a
+              >
+            </p>
+            <p class="mb-3.5 md:mb-5">
+              <a
+                href="/"
+                class="font-medium text-base md:text-lg text-[#8A8A8A] hover:text-[#1E1E1E]"
+                >Android application</a
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- FOOTER COPYRIGHT -->
+      <div class="pt-[20px] md:pt-[25px] pb-[35px] border-t border-[#E5F0F5]">
+        <p class="font-medium text-base text-[#1E1E1E] text-center">
+          Copyright © 2023 Travelers. All rights reserved.
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
